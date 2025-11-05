7e6eae33dfe447ae652e5180d07553dad89ade2f
SantiagoDgz <al45846@un.edu.mx>
2025-11-04 19:44:41 -0600

    fondo

--- calculos.js
+++ calculos.js
@@ -1,37 +1,51 @@
-var formulario = document.getElementById("calcularPrecio").value;
+var formulario = document.getElementById("calcularPrecio");
+
 function calcular() {
 
-    var cantidad = document.getElementById("cantidad").value
-    alert("Variable formulario:" + formulario + "Variable Cantidad:" + cantidad);
+
+    var cantidad = document.getElementById("cantidad").value;
+    //alert("Variable Formulario:" + formulario + "Variable cantidad:" + cantidad);
+ 
+         //var tamañoChico = document.getElementById("chico").value;
+    //alert(tamañoChico);
+
+        //var tamañoMediano = document.getElementById("mediano").value;
+    //alert(tamañoMediano);
+
+        //var tamañoGrande = document.getElementById("grande").value;
+    //alert(tamañoGrande);
+
+        var tamaño = formulario.tam.value
+        alert(tamaño);
     
-   // var tamChico = document.getElementById("chico").value;
-    //alert(tamChico);
+        var pan = formulario.pan.value
+        alert(pan);
 
-    //var tamMediano = document.getElementById("mediano").value;
-    //alert(tamMediano);
+        var combo = formulario.combo.value
+        alert(combo);
 
-    //var tamGrande = document.getElementById("grande").value;
-    //alert(tamGrande);
 
-    var tamaño = formulario.tam.value;
-
-    alert(tamaño);
 
     var total = 0;
 
-    if(tamaño == "chico")
-    {
-        total = 100;
-    }
-    else if (tamaño == "mediano")
-    {
-        total = 150;
-    }
-    else if (tamaño == "grande"){
-        total = 200;
-    }
+    if(tamaño == "chico"){
+        total = 100;}
+
+    if(tamaño == "mediano"){
+        total = 150;}
+
+    if(tamaño == "grande"){
+        total = 200;}
+
+
+    if(formulario.combo.value == "si") {
+        total = total + 75;     }  
+                                
+    if(formulario.combo.value == "no") {
+        total = total ;     }
+
 
     formulario.total.value = total * cantidad;
 
-    
+
 }
